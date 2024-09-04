8ec4407510ebe0df47be254513badc1f2267a0ca
vehktaur <victorakhihiero@gmail.com>
2024-09-04 11:24:12 +0100

    update

--- src/app/(admin-section)/layout.tsx
+++ src/app/(admin-section)/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`flex ${barlow.className}`}>
         <Sidebar />
-        <div className="grid min-h-screen w-full content-start">
+        <div className="grid min-full-screen w-full content-start">
           <nav className="h-[3.75rem] border-b border-black bg-stone-50 px-5 content-center">
             <div className="mx-auto flex max-w-6xl items-center justify-between">
               <h3 className="font-medium ~text-base/lg">Admin Panel</h3>
