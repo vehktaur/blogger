11f278bdd4ad9808aa0ced6dc7c7752901274c6b
vehktaur <victorakhihiero@gmail.com>
2024-09-04 11:46:46 +0100

    minor UI updates

--- src/app/(blog-app)/blogs/[title]/page.tsx
+++ src/app/(blog-app)/blogs/[title]/page.tsx
@@ -10,18 +10,18 @@ const Blog = ({ params }: { params: { title: string } }) => {
   return blog ? (
     <>
       <div className="bg-gray-200 pt-20">
-        <div className="px-5 pb-5 pt-16 text-center sm:~px-8/20">
-          <div className="mx-auto mb-28 max-w-[48rem]">
+        <div className="px-5 pb-5 ~pt-8/16 text-center sm:~px-8/20">
+          <div className="mx-auto ~mb-36/40 max-w-[48rem]">
             <h1 className="mb-6 text-2xl font-semibold sm:text-3xl md:text-5xl">
               {blog.title}
             </h1>
 
             <Image
-              className="mx-auto w-20 rounded-full border border-white [image-rendering:high-quality]"
+              className="mx-auto ~w-16/20 rounded-full border border-white [image-rendering:high-quality]"
               src={blog.author_img}
               alt={`${blog.author}`}
             />
-            <p className="mt-1 pb-2 font-medium italic text-[#333]">
+            <p className="mt-1 pb-2 font-medium italic text-[#333] ~text-sm/base">
               {blog.author}
             </p>
           </div>
