a8f39e45810228187560f90df51e6e3b43a69118
vehktaur <victorakhihiero@gmail.com>
2024-10-11 23:37:15 +0100

    check

--- src/app/(blog-app)/blog/[title]/page.tsx
+++ src/app/(blog-app)/blog/[title]/page.tsx
@@ -14,63 +14,63 @@ const Blog = async ({ params }: { params: { title: string } }) => {
 
   return blog ? (
     <>
-      <div className="bg-gray-200 pt-20">
-        <div className="px-5 pb-5 text-center ~pt-8/16 sm:~px-8/20">
-          <div className="mx-auto max-w-[48rem] ~mb-36/40">
-            <h1 className="mb-6 text-2xl font-bold sm:text-3xl md:text-5xl">
+      <div className='bg-gray-200 pt-20'>
+        <div className='px-5 pb-5 text-center ~pt-8/16 sm:~px-8/20'>
+          <div className='mx-auto max-w-[48rem] ~mb-36/40'>
+            <h1 className='mb-6 text-2xl font-bold sm:text-3xl md:text-5xl'>
               {blog.title}
             </h1>
 
             <Image
-              className="mx-auto rounded-full border border-white ~w-16/20 [image-rendering:high-quality]"
+              className='mx-auto rounded-full border border-white ~w-16/20 [image-rendering:high-quality]'
               src={assets.profile_img}
               alt={`${blog.author.name}`}
             />
-            <p className="mt-1 pb-2 font-medium italic text-[#333] ~text-sm/base">
+            <p className='mt-1 pb-2 font-medium italic text-[#333] ~text-sm/base'>
               {blog.author.name}
             </p>
           </div>
         </div>
       </div>
-      <div className="px-5 sm:~px-8/20">
-        <div className="mx-auto max-w-[50rem]">
-          <DynamicImage
-            className="mx-auto -mt-[6.25rem] mb-10 w-full border-4 border-white"
+      <div className='px-5 sm:~px-8/20'>
+        <div className='mx-auto max-w-[50rem]'>
+          <Image
+            className='mx-auto -mt-[6.25rem] mb-10 w-full border-4 border-white'
             src={blog.image.url}
             alt={blog.image.name}
             width={1280}
             height={720}
           />
 
-          <div className="prose ~mb-4/6 prose-h1:~text-2xl/3xl prose-h1:sm:text-4xl">
+          <div className='prose ~mb-4/6 prose-h1:~text-2xl/3xl prose-h1:sm:text-4xl'>
             <MDXRemote source={blog.content} />
           </div>
 
-          <div className="my-24">
-            <p className="font-semibold">Share this article on:</p>
-            <div className="mt-4 flex items-center gap-1">
-              <a href="#">
+          <div className='my-24'>
+            <p className='font-semibold'>Share this article on:</p>
+            <div className='mt-4 flex items-center gap-1'>
+              <a href='#'>
                 <Image
-                  src="/icons/x_icon.png"
+                  src='/icons/x_icon.png'
                   width={50}
                   height={50}
-                  alt="X Profile"
+                  alt='X Profile'
                 />
               </a>
-              <a href="#">
+              <a href='#'>
                 <Image
-                  src="/icons/facebook_icon.png"
+                  src='/icons/facebook_icon.png'
                   width={50}
                   height={50}
-                  alt="Github Profile"
+                  alt='Github Profile'
                 />
               </a>
-              <a href="#">
+              <a href='#'>
                 <Image
-                  src="/icons/googleplus_icon.png"
+                  src='/icons/googleplus_icon.png'
                   width={50}
                   height={50}
-                  alt="LinkedIn Profile"
+                  alt='LinkedIn Profile'
                 />
               </a>
             </div>
--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -2,15 +2,16 @@ import { Blog } from '@/lib/definitions';
 import { ChevronDoubleRightIcon } from '@heroicons/react/20/solid';
 import Link from 'next/link';
 import DynamicImage from './DynamicImage';
+import Image from 'next/image';
 
 const BlogCard = ({ blog }: { blog: Blog }) => {
   const url = `${blog.title}__${blog._id}`;
   return (
-    <Link className="grid" href={`/blog/${encodeURIComponent(url)}`}>
-      <div className="group flex h-full w-full max-w-[20.625rem] flex-col overflow-hidden rounded border border-black shadow transition-shadow duration-500 hover:shadow-offset sm:max-w-[18.75rem]">
-        <div className="border-b border-black">
-          <DynamicImage
-            className="w-full object-cover"
+    <Link className='grid' href={`/blog/${encodeURIComponent(url)}`}>
+      <div className='group flex h-full w-full max-w-[20.625rem] flex-col overflow-hidden rounded border border-black shadow transition-shadow duration-500 hover:shadow-offset sm:max-w-[18.75rem]'>
+        <div className='border-b border-black'>
+          <Image
+            className='w-full object-cover'
             src={blog.image.url}
             alt={blog.title}
             width={1280}
@@ -18,12 +19,12 @@ const BlogCard = ({ blog }: { blog: Blog }) => {
           />
         </div>
 
-        <div className="flex flex-col bg-white p-5">
-          <div className="mb-3 flex items-center gap-2">
+        <div className='flex flex-col bg-white p-5'>
+          <div className='mb-3 flex items-center gap-2'>
             {blog.categories.map((category) => (
               <span
                 key={`${category}_${blog._id}`}
-                className="rounded-[0.1rem] bg-black p-1 text-white ~text-xs/sm"
+                className='rounded-[0.1rem] bg-black p-1 text-white ~text-xs/sm'
               >
                 {category}
               </span>
@@ -31,20 +32,20 @@ const BlogCard = ({ blog }: { blog: Blog }) => {
           </div>
 
           <h3
-            className="mb-2 line-clamp-2 font-semibold tracking-tight text-gray-900"
+            className='mb-2 line-clamp-2 font-semibold tracking-tight text-gray-900'
             title={blog.title}
           >
             {blog.title}
           </h3>
 
-          <p className="line-clamp-3 text-sm tracking-tight text-gray-700">
+          <p className='line-clamp-3 text-sm tracking-tight text-gray-700'>
             {blog.description}
           </p>
         </div>
-        <div className="mb-5 mt-auto px-5">
-          <button className="flex items-center gap-1.5 font-bold ~text-sm/base">
+        <div className='mb-5 mt-auto px-5'>
+          <button className='flex items-center gap-1.5 font-bold ~text-sm/base'>
             Read more
-            <ChevronDoubleRightIcon className="mt-[0.025rem] w-4 transition-transform duration-700 group-hover:translate-x-1 group-hover:scale-x-[1.35]" />
+            <ChevronDoubleRightIcon className='mt-[0.025rem] w-4 transition-transform duration-700 group-hover:translate-x-1 group-hover:scale-x-[1.35]' />
           </button>
         </div>
       </div>
