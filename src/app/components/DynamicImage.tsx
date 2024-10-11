68a6d7c0f7e342c64bffea3cc4986b399d6d0ec8
vehktaur <victorakhihiero@gmail.com>
2024-10-11 23:28:25 +0100

    check update

--- src/app/components/DynamicImage.tsx
+++ src/app/components/DynamicImage.tsx
@@ -1,7 +1,11 @@
 import { getBlurData } from '@/lib/utils';
 import Image, { type ImageProps } from 'next/image';
 
-const DynamicImage = async (props: ImageProps) => {
+const DynamicImage = async ({
+  placeholder,
+  blurDataURL,
+  ...props
+}: ImageProps) => {
   const base64 = await getBlurData(props.src as string);
 
   return <Image {...props} placeholder='blur' blurDataURL={base64} />;
