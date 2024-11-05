19216f1198533b6fc3d86ce96c6cb65f545d3cb5
vehktaur <victorakhihiero@gmail.com>
2024-11-05 17:18:29 +0100

    remove fancy mongodb connection

--- src/lib/config/db.ts
+++ src/lib/config/db.ts
@@ -6,16 +6,16 @@ const connection = { isConnected: mongoose.connection?.readyState };
 
 export const ConnectDB = async () => {
   try {
-    if (connection.isConnected === 1) {
-      console.log('DB already connected');
-      return mongoose.connection.getClient(); // Return the MongoDB client if already connected
-    }
+    // if (connection.isConnected === 1) {
+    //   console.log('DB already connected');
+    //   return mongoose.connection.getClient(); // Return the MongoDB client if already connected
+    // }
 
     await mongoose.connect(apiUrl!); // Establish the connection
     console.log('DB connected successfully');
 
-    connection.isConnected = mongoose.connection.readyState;
-    return mongoose.connection.getClient(); // Return the MongoDB client
+   // connection.isConnected = mongoose.connection.readyState;
+  //  return mongoose.connection.getClient(); // Return the MongoDB client
   } catch (error) {
     console.log('Could not connect to DB');
     console.log(error);
