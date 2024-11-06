046ac3427eca6ff2d6d56a3cdd8961a522b57d66
vehktaur <victorakhihiero@gmail.com>
2024-11-06 11:20:23 +0100

    remove isconnected check entirely

--- src/lib/config/db.ts
+++ src/lib/config/db.ts
@@ -4,21 +4,13 @@ const apiUrl = process.env.MONGODB_URI;
 
 export const ConnectDB = async () => {
   try {
-    if (mongoose.connection.readyState >= 1) {
-      console.log('DB already connected');
-      return mongoose.connection.getClient(); // Already connected
-    }
-
     if (!apiUrl) {
       console.error('No MongoDB URI found in environment variables.');
       return null;
     }
 
-    // Connect to MongoDB with options
     await mongoose.connect(apiUrl);
     console.log('DB connected successfully');
-
-    return mongoose.connection.getClient(); // Return the client for verification
   } catch (error) {
     console.error('Could not connect to DB:', error);
     return null;
