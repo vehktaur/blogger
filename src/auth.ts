ab4d8b1dfa9200c47c020c4591a1000222c1c2f6
vehktaur <victorakhihiero@gmail.com>
2024-11-09 06:41:30 +0100

    check

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,23 @@ import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
 import Credentials from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
-import { User } from './lib/models/UserModel';
-import { getUser } from './lib/utils';
+
+const getFakeUser = () => {
+  let user = {
+    firstName: 'Victor',
+    lastName: 'Akhihiero',
+    name: 'Victor Akhihiero',
+    email: 'victorakhihiero@gmail.com',
+    username: 'vehktaur',
+    _id: '',
+    role: 'user',
+    password: '$2a$10$IUq8yxxA/NlCeNZs08pSg.khLNob9KLA6eTLM3j.uaqRA5SaKRAgi',
+    createdAt: new Date('2024-11-02T10:49:26.345+00:00'),
+    updatedAt: new Date('2024-11-02T10:49:26.345+00:00'),
+  };
+
+  return user;
+};
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -17,7 +32,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         try {
           const { email, password } = credentials;
 
-          const user = (await getUser({ email: String(email) })) as User;
+          // const user = (await getUser({ email: String(email) })) as User;
+
+          const user = getFakeUser();
 
           if (!user) {
             throw new Error('User not found');
